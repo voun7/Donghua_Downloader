84f922b91cb5a1945107e94ee99693040cb94ecf
voun-github <nwaeze_victor@yahoo.com>
2022-11-11 17:10:10 -0500

    removed name generator and added multithreading for video downloads

--- scraper.py
+++ scraper.py
@@ -1,3 +1,4 @@
+import concurrent.futures
 import logging
 import re
 import time
@@ -55,7 +56,7 @@ class XiaoheimiScraper:
                 latest_video_number = int(post_update[0].strip('更新至集全'))
                 video_start_num = latest_video_number - default_num_videos + 1
                 logger.info(f"Post named: {name} is new, latest video number: {latest_video_number}. "
-                            f"Last {default_num_videos} video numbers: {video_start_num} - {latest_video_number}")
+                            f"Last {default_num_videos} video numbers: {video_start_num}-{latest_video_number}")
                 for video_number in range(video_start_num, latest_video_number + 1):
                     video_post = soup.find('li', {"title": f"{video_number}"})
                     video_link = self.base_url + video_post.find('a').get('href')
@@ -83,40 +84,50 @@ class XiaoheimiScraper:
         return checked_video_urls
 
     @staticmethod
-    def file_name_generator(file_name: str) -> str:
-        new_name = file_name.strip(' 在线播放 - 小宝影院 - 在线视频')
-        for match in re.finditer(r'(\d+)', new_name):
-            number = match.group(0)
-            new_name = new_name.replace(number, f"第{number}集")
-        return new_name
+    def download_video(download_link: str, file_name: str, download_location: Path) -> None:
+        def my_hook(d: dict) -> None:
+            if d['status'] == 'error':
+                logger.exception('An error has occurred ...')
+            if d['status'] == 'finished':
+                logger.info('Done downloading file, now post-processing ...')
+
+        ydl_opts = {
+            'logger': logger.getChild('yt_dlp'),
+            'progress_hooks': [my_hook],
+            'noprogress': True,
+            'ignoreerrors': True,
+            'socket_timeout': 120,
+            'wait_for_video': (1, 600),
+            'download_archive': 'logs/yt_dlp_downloads_archive.txt',
+            'ffmpeg_location': 'ffmpeg/bin',
+            'outtmpl': str(download_location) + '/' + file_name + '.%(ext)s'
+        }
+        with YoutubeDL(ydl_opts) as ydl:
+            ydl.download(download_link)
 
     # This method uses the video url to find the video download link.
     # It uses yt-dlp to download the file from hls stream
-    def video_downloader(self, video_urls: list, download_location: Path) -> None:
+    def video_downloader(self, video_url: str, download_location: Path) -> None:
+        page_response = requests.get(video_url, headers=self.header)
+        soup = BeautifulSoup(page_response.text, 'lxml')
+        file_name = soup.title.string.strip(' 在线播放 - 小宝影院 - 在线视频')
+        for match in re.finditer(r'(\d+)', file_name):
+            number = match.group(0)
+            file_name = file_name.replace(number, f"第{number}集")
+        download_script = soup.find(class_='embed-responsive clearfix')
+        download_match = re.finditer(r'"url":"(.*?)"', str(download_script))
+        download_link = None
+        for match in download_match:
+            download_link = match[1].replace("\\", '')
+        logger.info(f"Downloading Post: {video_url}, File name: {file_name}")
+        logger.debug(f"Download link: {download_link}")
+        self.download_video(download_link, file_name, download_location)
+
+    def download_all_videos(self, video_urls: list, download_location: Path) -> None:
         logger.info("..........Downloading matched recent site videos..........")
         start = time.perf_counter()
-        for url in video_urls:
-            page_response = requests.get(url, headers=self.header)
-            soup = BeautifulSoup(page_response.text, 'lxml')
-            file_name = self.file_name_generator(soup.title.string)
-            download_script = soup.find(class_='embed-responsive clearfix')
-            download_match = re.finditer(r'"url":"(.*?)"', str(download_script))
-            download_link = None
-            for match in download_match:
-                download_link = match[1].replace("\\", '')
-                logger.debug(download_link)
-            logger.info(f"Downloading Post: {url}, File name: {file_name}")
-
-            ydl_opts = {
-                'logger': logger.getChild('yt_dlp'),
-                'noprogress': True,
-                'ignoreerrors': True,
-                'download_archive': 'logs/yt_dlp_downloads_archive.txt',
-                'ffmpeg_location': 'ffmpeg/bin',
-                'outtmpl': str(download_location) + '/' + file_name + '.%(ext)s'
-            }
-            with YoutubeDL(ydl_opts) as ydl:
-                ydl.download(download_link)
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            _ = [executor.submit(self.video_downloader, url, download_location) for url in video_urls]
         end = time.perf_counter()
         total_time = end - start
         logger.info(f"Downloads finished Total time: {total_time}")

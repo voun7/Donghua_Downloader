74bf8c7c4cd6b3e2300ad98088301c2327dffd7f
voun-github <nwaeze_victor@yahoo.com>
2022-11-12 18:42:03 -0500

    variable changed

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def main() -> None:
     ]
     playlist_id = "PLdUiOF8vZ51jW1w84E01SGY2KNeOEPZBn"
     anime_list = [keyword for folder in destination_dir.iterdir() for keyword in re.findall(r'\((.*?)\)', folder.name)]
-    anime_list_two = ["徒弟个个是大佬", "徒弟都是女魔头", "被迫成为反派赘婿", "异皇重生", "丹武至尊", "祖师出山"]
+    anime_list_two = ["徒弟个个是大佬", "徒弟都是女魔头", "被迫成为反派赘婿", "异皇重生", "妖道至尊", "祖师出山"]
 
     youtube = YouTube(playlist_id)
     youtube.clear_playlist()

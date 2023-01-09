17d06dd6228db36d55234762acaf36fa6726dbf9
victor n <nwaeze_victor@yahoo.com>
2023-01-09 07:10:04 -0500

    anime's and channel id's updated

--- main.py
+++ main.py
@@ -27,22 +27,23 @@ def main() -> None:
     playlist_download_dir = Path(r"\\192.168.0.111\General File Sharing\From YouTube\Chinese Anime For Subbing")
     destination_dir = playlist_download_dir / "##Currently Airing"
     playlist_id = "PLdUiOF8vZ51jW1w84E01SGY2KNeOEPZBn"
-    youtube_channel_ids = ["UC80ztI40QAXzWL94eoRzWow", "UCBIiQ5Hlsadi1HQl8dufZag", "UC8r57bRU8OrpXnLFNC0ym7Q"]
     anime_list = [keyword for folder in destination_dir.iterdir() for keyword in re.findall(r'\((.*?)\)', folder.name)]
-    anime_list_two = ["徒弟个个是大佬", "徒弟都是女魔头", "被迫成为反派赘婿", "异皇重生", "妖道至尊", "祖师出山"]
+
+    youtube_channel_ids = ["UC80ztI40QAXzWL94eoRzWow", "UCBIiQ5Hlsadi1HQl8dufZag", "UC8r57bRU8OrpXnLFNC0ym7Q"]
+
+    xh_anime_list = ["徒弟个个是大佬", "徒弟都是女魔头", "被迫成为反派赘婿", "异皇重生", "妖道至尊", "祖师出山", "仙武帝尊", "龙城争霸",
+                     "九霄帝神"]
+    yt_anime_list = filter_anime_list(anime_list, xh_anime_list)
 
     # Arguments
     youtube = YouTube(playlist_id)
     youtube.clear_playlist()
-    youtube.match_to_youtube_videos(youtube_channel_ids, anime_list)
-    youtube.match_to_youtube_videos(
-        ["UCJSAZ5pbDi8StbSbJI1riEg", "UCJS5PJXcAIpXkBOjPNvK7Uw"],
-        ["从姑获鸟开始", "掌门低调点", "大主宰"]
-    )
+    youtube.match_to_youtube_videos(youtube_channel_ids, yt_anime_list)
+    youtube.match_to_youtube_videos(["UCJS5PJXcAIpXkBOjPNvK7Uw"], ["大主宰"])
     youtube.playlist_downloader(playlist_download_dir)
 
     xiaoheimi = XiaoheimiScraper()
-    matched_urls = xiaoheimi.match_to_recent_videos(anime_list_two)
+    matched_urls = xiaoheimi.match_to_recent_videos(xh_anime_list)
     xiaoheimi.download_all_videos(matched_urls, playlist_download_dir)
 
     logger.debug("Logging Ended\n")

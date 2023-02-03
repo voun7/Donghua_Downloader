ece669437de1dd2cc978eb2e25ede45e8173a2ec
victor n <nwaeze_victor@yahoo.com>
2023-02-03 12:27:57 -0500

    program will be updated before running.

---
+++ run.py
@@ -0,0 +1,27 @@
+import logging
+import subprocess
+
+from logger_setup import get_log
+
+logger = logging.getLogger(__name__)
+
+
+def update_and_run():
+    logger.debug("Checking main git repository for updates...")
+
+    result = subprocess.run(["git", "pull"], capture_output=True)
+
+    if "Already up to date" in str(result.stdout):
+        logger.info(f"Local repository is up to date. {result}")
+    else:
+        logger.warning(f"Local repository is not up to date. {result}")
+
+    from main import main
+    main()
+
+
+if __name__ == '__main__':
+    get_log()
+    logger.debug("Logging Started")
+    update_and_run()
+    logger.debug("Logging Ended\n")

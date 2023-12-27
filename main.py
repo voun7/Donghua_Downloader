78dd6e9f9776477a54a8bb777b2a8d4aeb96e463
Victor N <nwaeze_victor@yahoo.com>
2023-12-27 05:59:53 +0100

    new YouTube channels

--- main.py
+++ main.py
@@ -61,9 +61,11 @@ def run_youtube_api(yt_dl_archive_file: Path, resolved_names_file: Path, anime_l
     ch_id_6 = "UCXmOpN9pg1hJBRkHODL00EA"  # 三福动漫 Sanfu
     ch_id_7 = "UCNIKva6iDURgVxf44pMZlKA"  # Animal Family
     ch_id_8 = "UCgVZKHiJrIHomPGMD8KYTzw"  # D-ANIME
-    ch_id_9 = "UCVSQzmMRQedQRCZZU8QxYow"  # 知名动漫
+    ch_id_9 = "UC_JsHod-IAlWFWi7kDhb03Q"  # 阅文漫画
     ch_id_10 = "UCQKq8mAjHXFRd6CsdDrCB1w"  # Ziyue Animation
-    youtube_channel_ids = [ch_id_1, ch_id_2, ch_id_3, ch_id_4, ch_id_5, ch_id_6, ch_id_7, ch_id_8, ch_id_9, ch_id_10]
+    ch_id_11 = "UC_grnC_fPff0bSbk9V-R3aQ"  # 幻月动漫 Moon Anime
+    youtube_channel_ids = [ch_id_1, ch_id_2, ch_id_3, ch_id_4, ch_id_5, ch_id_6, ch_id_7, ch_id_8, ch_id_9, ch_id_10,
+                           ch_id_11]
     yd = YouTubeDownloader(yt_dl_archive_file)
 
     try:

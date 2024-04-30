e0feb7aeb45452957c6dd808c347131dc4b92455
Victor N <nwaeze_victor@yahoo.com>
2024-04-30 19:10:54 -0400

    88mv temp disable

--- main.py
+++ main.py
@@ -157,20 +157,20 @@ def run_scrappers(resolved_names_file: Path, tb: TelegramBot) -> None:
         logger.exception(error_message)
         tb.send_telegram_message(error_message)
 
-    site_address = "88mv.tv"
-    try:
-        site_address = um.check_url(site_address)
-        logger.info(f"Checking {site_address} site for recent anime upload matches...")
-        eight_eight_mv = EightEightMVScrapper(site_address)
-        site_posts = eight_eight_mv.get_anime_posts()
-        site_posts.update(eight_eight_mv.get_anime_posts(page=2))
-        matched_posts = eight_eight_mv.match_to_recent_videos(site_posts)
-        matched_download_details = eight_eight_mv.get_recent_posts_videos_download_link(matched_posts)
-        sd.batch_downloader(site_address, matched_download_details)
-    except Exception as error:
-        error_message = f"An error occurred while running {site_address} site scrapper! \nError: {error}"
-        logger.exception(error_message)
-        tb.send_telegram_message(error_message)
+    # site_address = "88mv.tv"
+    # try:
+    #     site_address = um.check_url(site_address)
+    #     logger.info(f"Checking {site_address} site for recent anime upload matches...")
+    #     eight_eight_mv = EightEightMVScrapper(site_address)
+    #     site_posts = eight_eight_mv.get_anime_posts()
+    #     site_posts.update(eight_eight_mv.get_anime_posts(page=2))
+    #     matched_posts = eight_eight_mv.match_to_recent_videos(site_posts)
+    #     matched_download_details = eight_eight_mv.get_recent_posts_videos_download_link(matched_posts)
+    #     sd.batch_downloader(site_address, matched_download_details)
+    # except Exception as error:
+    #     error_message = f"An error occurred while running {site_address} site scrapper! \nError: {error}"
+    #     logger.exception(error_message)
+    #     tb.send_telegram_message(error_message)
 
     site_address = "imyyds.com"
     try:

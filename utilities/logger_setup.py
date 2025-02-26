b0870e82c882aeebcea2c6ab4d67e265ce604903
Victor N <nwaeze_victor@yahoo.com>
2025-02-26 09:41:52 -0500

    logger setup updated

--- utilities/logger_setup.py
+++ utilities/logger_setup.py
@@ -4,30 +4,38 @@ from logging.handlers import TimedRotatingFileHandler, SysLogHandler
 from pathlib import Path
 
 
-def get_console_handler() -> logging.handlers:
-    console_handler = logging.StreamHandler(sys.stdout)
-    console_handler.setLevel(logging.INFO)
-    # The console sends only messages by default no need for formatter.
-    return console_handler
-
-
-def get_server_handler(log_format: logging.Formatter) -> logging.handlers:
+def console_handler() -> logging.handlers:
     """
-    Sends logs to the graylog server.
+    Sends logs to the console.
+    The console sends only messages by default no need for formatter.
     """
-    server_handler = SysLogHandler(address=('192.168.31.114', 1514))
-    server_handler.setLevel(logging.INFO)
-    server_handler.setFormatter(log_format)
-    return server_handler
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.INFO)
+    return handler
 
 
-def get_file_handler(log_dir: Path, log_format: logging.Formatter) -> logging.handlers:
+def server_handler(log_format: logging.Formatter) -> logging.handlers:
+    """
+    Sends logs to the syslog server.
+    """
+    handler = SysLogHandler(('192.168.31.114', 1514))
+    handler.setLevel(logging.INFO)
+    handler.setFormatter(log_format)
+    return handler
+
+
+def file_handler(log_format: logging.Formatter) -> logging.handlers:
+    """
+    Sends logs to the log file.
+    """
+    log_dir = Path(__file__).parent.parent / "logs"
+    log_dir.mkdir(exist_ok=True)
     log_file = log_dir / "runtime.log"
-    file_handler = TimedRotatingFileHandler(log_file, when='H', interval=6, backupCount=14, encoding='utf-8')
-    file_handler.namer = my_namer
-    file_handler.setLevel(logging.DEBUG)
-    file_handler.setFormatter(log_format)
-    return file_handler
+    handler = TimedRotatingFileHandler(log_file, 'H', 6, 20, 'utf-8')
+    handler.namer = log_namer
+    handler.setLevel(logging.DEBUG)
+    handler.setFormatter(log_format)
+    return handler
 
 
 def setup_logging() -> None:
@@ -39,24 +47,18 @@ def setup_logging() -> None:
     The following suppress log messages. It will not log messages of given module unless they are at least warnings.
     logging.getLogger("").setLevel(logging.WARNING)
     """
-    # Create folder for file logs.
-    log_dir = Path(__file__).parent.parent / "logs"
-    log_dir.mkdir(exist_ok=True)
-
     # Create a custom logger.
     logger = logging.getLogger()
     logger.setLevel(logging.DEBUG)
-
     # Create formatters and add it to handlers.
     log_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
     # Add handlers to the logger.
-    logger.addHandler(get_console_handler())
-    logger.addHandler(get_server_handler(log_format))
-    logger.addHandler(get_file_handler(log_dir, log_format))
+    logger.addHandler(console_handler())
+    logger.addHandler(server_handler(log_format))
+    logger.addHandler(file_handler(log_format))
 
 
-def my_namer(default_name: str) -> str:
+def log_namer(default_name: str) -> str:
     """
     This will be called when doing the log rotation
     default_name is the default filename that would be assigned, e.g. Rotate_Test.txt.YYYY-MM-DD
